4a8d2773191071d7cc7b8eb3348f42163167140d
luisftec <luisftec2022@gmail.com>
2023-12-30 20:00:13 -0500

    commit para conectar con netlify 3

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router >
+        <Router basename="/academia-legan">
             <div className="App">
                 <Navbar />
                 <Routes>
